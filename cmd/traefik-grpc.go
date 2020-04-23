bf8ab2d15cd9ff2a0ab9de062cf9ab15b7166ad2
张重阳 <zhangchongyang@variflight.com>
2020-04-23 19:24:29 +0800

    为proto文件字符串增加压缩

--- cmd/traefik-grpc.go
+++ cmd/traefik-grpc.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/base64"
 	"fmt"
 	"github.com/golang/protobuf/proto"
@@ -10,12 +12,11 @@ import (
 	"path/filepath"
 )
 
-
-func main()  {
+func main() {
 	files := []string{}
 	paths := []string{}
 
-	filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error)error{
+	filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			files = append(files, path)
 		}
@@ -25,10 +26,14 @@ func main()  {
 	x := protoparse.Parser{ImportPaths: paths}
 	fs, _ := x.ParseFiles(files...)
 	set := new(descriptor.FileDescriptorSet)
-	for _, item := range fs{
+	for _, item := range fs {
 		set.File = append(set.File, item.AsFileDescriptorProto())
 	}
 
 	bts, _ := proto.Marshal(set)
-	fmt.Println(base64.StdEncoding.EncodeToString(bts))
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	w.Write(bts)
+	w.Close()
+	fmt.Println(base64.StdEncoding.EncodeToString(b.Bytes()))
 }
